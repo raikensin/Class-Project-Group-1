73ca9d90d3920a041bf1d19e3fd36f61529e94ef
j0515017 <j0515017@amarillocollege.com>
2017-04-25 19:44:40 -0500

    I said hi

--- WinFormTest/WinFormTest/Program.cs
+++ WinFormTest/WinFormTest/Program.cs
@@ -18,6 +18,7 @@ namespace WinFormTest
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // hey!!!!! 
         }
     }
 }
