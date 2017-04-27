24600b28349902b83a4ce844b452f8bf4037ae83
j0515017 <j0515017@amarillocollege.com>
2017-04-27 17:03:43 -0500

    Added a orders class for the ordernumbers and the actual order placed

---
+++ WinFormTest/WinFormTest/Orders.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFormTest
+{
+    public class Orders : Items
+    {
+        private int orderNumber;
+        private string order;
+
+        public int OrderNumber
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string Order
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void AddOrders()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EditOrders()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
