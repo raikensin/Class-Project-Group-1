8476459963478d46a8fac31c722d22a239c55436
Kyla Collier <kjcollier23@amarillocollege.com>
2017-05-02 18:31:18 -0500

    named the buttons in the floor plan

--- RestaurantManagement/RestaurantManagement/FloorPlan.Designer.cs
+++ RestaurantManagement/RestaurantManagement/FloorPlan.Designer.cs
@@ -28,102 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
+            this.btnTable1 = new System.Windows.Forms.Button();
+            this.btnTable2 = new System.Windows.Forms.Button();
+            this.btnTable3 = new System.Windows.Forms.Button();
+            this.btnTable4 = new System.Windows.Forms.Button();
+            this.btnTable5 = new System.Windows.Forms.Button();
+            this.btnTable6 = new System.Windows.Forms.Button();
+            this.btnReservation = new System.Windows.Forms.Button();
+            this.btnWaitingList = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTable1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 18);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 118);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Table 1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTable1.Location = new System.Drawing.Point(19, 15);
+            this.btnTable1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable1.Name = "btnTable1";
+            this.btnTable1.Size = new System.Drawing.Size(91, 96);
+            this.btnTable1.TabIndex = 0;
+            this.btnTable1.Text = "Table 1";
+            this.btnTable1.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnTable2
             // 
-            this.button2.Location = new System.Drawing.Point(25, 156);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 118);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Table 2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnTable2.Location = new System.Drawing.Point(19, 127);
+            this.btnTable2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable2.Name = "btnTable2";
+            this.btnTable2.Size = new System.Drawing.Size(91, 96);
+            this.btnTable2.TabIndex = 1;
+            this.btnTable2.Text = "Table 2";
+            this.btnTable2.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnTable3
             // 
-            this.button3.Location = new System.Drawing.Point(25, 296);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 118);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Table 3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnTable3.Location = new System.Drawing.Point(19, 240);
+            this.btnTable3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable3.Name = "btnTable3";
+            this.btnTable3.Size = new System.Drawing.Size(91, 96);
+            this.btnTable3.TabIndex = 2;
+            this.btnTable3.Text = "Table 3";
+            this.btnTable3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnTable4
             // 
-            this.button4.Location = new System.Drawing.Point(174, 18);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(121, 118);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Table 4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.btnTable4.Location = new System.Drawing.Point(130, 15);
+            this.btnTable4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable4.Name = "btnTable4";
+            this.btnTable4.Size = new System.Drawing.Size(91, 96);
+            this.btnTable4.TabIndex = 3;
+            this.btnTable4.Text = "Table 4";
+            this.btnTable4.UseVisualStyleBackColor = true;
+            this.btnTable4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // btnTable5
             // 
-            this.button5.Location = new System.Drawing.Point(174, 156);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(121, 118);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Table 5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnTable5.Location = new System.Drawing.Point(130, 127);
+            this.btnTable5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable5.Name = "btnTable5";
+            this.btnTable5.Size = new System.Drawing.Size(91, 96);
+            this.btnTable5.TabIndex = 4;
+            this.btnTable5.Text = "Table 5";
+            this.btnTable5.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnTable6
             // 
-            this.button6.Location = new System.Drawing.Point(174, 296);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(121, 118);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "Table 6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnTable6.Location = new System.Drawing.Point(130, 240);
+            this.btnTable6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTable6.Name = "btnTable6";
+            this.btnTable6.Size = new System.Drawing.Size(91, 96);
+            this.btnTable6.TabIndex = 5;
+            this.btnTable6.Text = "Table 6";
+            this.btnTable6.UseVisualStyleBackColor = true;
             // 
-            // button7
+            // btnReservation
             // 
-            this.button7.Location = new System.Drawing.Point(554, 38);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(154, 39);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "Reservation";
-            this.button7.UseVisualStyleBackColor = true;
+            this.btnReservation.Location = new System.Drawing.Point(416, 31);
+            this.btnReservation.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnReservation.Name = "btnReservation";
+            this.btnReservation.Size = new System.Drawing.Size(116, 32);
+            this.btnReservation.TabIndex = 6;
+            this.btnReservation.Text = "Reservation";
+            this.btnReservation.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // btnWaitingList
             // 
-            this.button8.Location = new System.Drawing.Point(554, 156);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(154, 39);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "Waiting List";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btnWaitingList.Location = new System.Drawing.Point(416, 127);
+            this.btnWaitingList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnWaitingList.Name = "btnWaitingList";
+            this.btnWaitingList.Size = new System.Drawing.Size(116, 32);
+            this.btnWaitingList.TabIndex = 7;
+            this.btnWaitingList.Text = "Waiting List";
+            this.btnWaitingList.UseVisualStyleBackColor = true;
             // 
             // FloorPlan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 507);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(598, 412);
+            this.Controls.Add(this.btnWaitingList);
+            this.Controls.Add(this.btnReservation);
+            this.Controls.Add(this.btnTable6);
+            this.Controls.Add(this.btnTable5);
+            this.Controls.Add(this.btnTable4);
+            this.Controls.Add(this.btnTable3);
+            this.Controls.Add(this.btnTable2);
+            this.Controls.Add(this.btnTable1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FloorPlan";
             this.Text = "FloorPlan";
             this.Load += new System.EventHandler(this.FloorPlan_Load);
@@ -133,13 +142,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnTable1;
+        private System.Windows.Forms.Button btnTable2;
+        private System.Windows.Forms.Button btnTable3;
+        private System.Windows.Forms.Button btnTable4;
+        private System.Windows.Forms.Button btnTable5;
+        private System.Windows.Forms.Button btnTable6;
+        private System.Windows.Forms.Button btnReservation;
+        private System.Windows.Forms.Button btnWaitingList;
     }
 }
