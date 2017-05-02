f23b2f8cd534dd86a754904546507c1209c5db8d
Kyla Collier <kjcollier23@amarillocollege.com>
2017-05-02 18:56:48 -0500

    upadated Manage form with button names and text fields KC

--- RestaurantManagement/RestaurantManagement/Manage.Designer.cs
+++ RestaurantManagement/RestaurantManagement/Manage.Designer.cs
@@ -29,184 +29,264 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Manage));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnAssignTables = new System.Windows.Forms.Button();
+            this.btnRemoveEmployee = new System.Windows.Forms.Button();
+            this.btnAddEmployee = new System.Windows.Forms.Button();
+            this.btnmenu = new System.Windows.Forms.Button();
+            this.btnLogOut = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.btnHostBusBoy = new System.Windows.Forms.Button();
+            this.btnCook = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnEmployees
             // 
-            this.button1.Location = new System.Drawing.Point(879, 43);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(218, 53);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Employees";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnEmployees.Location = new System.Drawing.Point(659, 35);
+            this.btnEmployees.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(164, 43);
+            this.btnEmployees.TabIndex = 0;
+            this.btnEmployees.Text = "Employees";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            this.btnEmployees.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnAssignTables
             // 
-            this.button2.Location = new System.Drawing.Point(879, 295);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(218, 53);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAssignTables.Location = new System.Drawing.Point(659, 189);
+            this.btnAssignTables.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAssignTables.Name = "btnAssignTables";
+            this.btnAssignTables.Size = new System.Drawing.Size(164, 50);
+            this.btnAssignTables.TabIndex = 1;
+            this.btnAssignTables.Text = "Assign Tables";
+            this.btnAssignTables.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnRemoveEmployee
             // 
-            this.button3.Location = new System.Drawing.Point(879, 205);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(218, 53);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnRemoveEmployee.Location = new System.Drawing.Point(659, 139);
+            this.btnRemoveEmployee.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemoveEmployee.Name = "btnRemoveEmployee";
+            this.btnRemoveEmployee.Size = new System.Drawing.Size(164, 44);
+            this.btnRemoveEmployee.TabIndex = 2;
+            this.btnRemoveEmployee.Text = "Remove Emoloyee";
+            this.btnRemoveEmployee.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnAddEmployee
             // 
-            this.button4.Location = new System.Drawing.Point(879, 111);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(218, 53);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnAddEmployee.Location = new System.Drawing.Point(659, 87);
+            this.btnAddEmployee.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAddEmployee.Name = "btnAddEmployee";
+            this.btnAddEmployee.Size = new System.Drawing.Size(164, 43);
+            this.btnAddEmployee.TabIndex = 3;
+            this.btnAddEmployee.Text = "Add Employee";
+            this.btnAddEmployee.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnmenu
             // 
-            this.button5.Location = new System.Drawing.Point(347, 451);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(218, 53);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnmenu.Location = new System.Drawing.Point(194, 337);
+            this.btnmenu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnmenu.Name = "btnmenu";
+            this.btnmenu.Size = new System.Drawing.Size(143, 50);
+            this.btnmenu.TabIndex = 4;
+            this.btnmenu.Text = "Menu";
+            this.btnmenu.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // btnLogOut
             // 
-            this.button6.Location = new System.Drawing.Point(879, 390);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(218, 53);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.btnLogOut.Location = new System.Drawing.Point(659, 248);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(164, 50);
+            this.btnLogOut.TabIndex = 5;
+            this.btnLogOut.Text = "Logout";
+            this.btnLogOut.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(33, 27);
+            this.pictureBox1.Location = new System.Drawing.Point(25, 22);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(275, 223);
+            this.pictureBox1.Size = new System.Drawing.Size(206, 181);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.txtStartDate);
+            this.groupBox1.Controls.Add(this.txtPosition);
+            this.groupBox1.Controls.Add(this.txtPhoneNumber);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtLastName);
+            this.groupBox1.Controls.Add(this.txtFirstName);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(333, 27);
+            this.groupBox1.Location = new System.Drawing.Point(250, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(511, 338);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(383, 275);
             this.groupBox1.TabIndex = 19;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Employees Information";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(33, 238);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(83, 17);
-            this.label8.TabIndex = 15;
-            this.label8.Text = "Description:";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 52);
+            this.label2.Location = new System.Drawing.Point(32, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.Size = new System.Drawing.Size(60, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "First Name:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(42, 81);
+            this.label7.Location = new System.Drawing.Point(32, 66);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(76, 17);
+            this.label7.Size = new System.Drawing.Size(58, 13);
             this.label7.TabIndex = 14;
             this.label7.Text = "Last Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 206);
+            this.label3.Location = new System.Drawing.Point(30, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Start Date:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(54, 113);
+            this.label6.Location = new System.Drawing.Point(40, 92);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 17);
+            this.label6.Size = new System.Drawing.Size(48, 13);
             this.label6.TabIndex = 13;
             this.label6.Text = "Address:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(54, 171);
+            this.label4.Location = new System.Drawing.Point(40, 139);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 11;
             this.label4.Text = "Position:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(11, 139);
+            this.label5.Location = new System.Drawing.Point(8, 113);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.Size = new System.Drawing.Size(81, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Phone Number:";
             // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(97, 39);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(200, 20);
+            this.txtFirstName.TabIndex = 16;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(97, 65);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(200, 20);
+            this.txtLastName.TabIndex = 17;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(97, 91);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 20);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(97, 117);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(200, 20);
+            this.txtPhoneNumber.TabIndex = 19;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(97, 143);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(200, 20);
+            this.txtPosition.TabIndex = 20;
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Location = new System.Drawing.Point(97, 169);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.Size = new System.Drawing.Size(200, 20);
+            this.txtStartDate.TabIndex = 21;
+            // 
+            // btnHostBusBoy
+            // 
+            this.btnHostBusBoy.Location = new System.Drawing.Point(356, 337);
+            this.btnHostBusBoy.Name = "btnHostBusBoy";
+            this.btnHostBusBoy.Size = new System.Drawing.Size(147, 50);
+            this.btnHostBusBoy.TabIndex = 20;
+            this.btnHostBusBoy.Text = "Host and Bus";
+            this.btnHostBusBoy.UseVisualStyleBackColor = true;
+            // 
+            // btnCook
+            // 
+            this.btnCook.Location = new System.Drawing.Point(518, 337);
+            this.btnCook.Name = "btnCook";
+            this.btnCook.Size = new System.Drawing.Size(147, 50);
+            this.btnCook.TabIndex = 21;
+            this.btnCook.Text = "Cook";
+            this.btnCook.UseVisualStyleBackColor = true;
+            // 
             // Manage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1126, 532);
+            this.ClientSize = new System.Drawing.Size(844, 432);
+            this.Controls.Add(this.btnCook);
+            this.Controls.Add(this.btnHostBusBoy);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.btnmenu);
+            this.Controls.Add(this.btnAddEmployee);
+            this.Controls.Add(this.btnRemoveEmployee);
+            this.Controls.Add(this.btnAssignTables);
+            this.Controls.Add(this.btnEmployees);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Manage";
             this.Text = "Manage";
             this.Load += new System.EventHandler(this.Manage_Load);
@@ -219,20 +299,27 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnAssignTables;
+        private System.Windows.Forms.Button btnRemoveEmployee;
+        private System.Windows.Forms.Button btnAddEmployee;
+        private System.Windows.Forms.Button btnmenu;
+        private System.Windows.Forms.Button btnLogOut;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Button btnHostBusBoy;
+        private System.Windows.Forms.Button btnCook;
     }
 }
