cf1ad0917bd7c12d0aa436f0f7321b33d113afc5
j0515017 <j0515017@amarillocollege.com>
2017-05-02 19:53:41 -0500

    Added button to menu and set up some methods in items to use in menu form

--- RestaurantManagement/RestaurantManagement/Classes/Item.cs
+++ RestaurantManagement/RestaurantManagement/Classes/Item.cs
@@ -5,35 +5,82 @@ using System.Text;
 
 namespace RestaurantManagement
 {
-    public class Item
+    public class Item : Menu
     {
-        private string food;
-        private double cost;
+        private string hamburger = " Hamburger ";
+        private string doublepatty= " Double Patty ";
+        private string tomato= " Tomato ";
+        private string onions= " Onions ";
+        private string letttuce= " Lettuce ";
+        private string pickle = " Pickle ";
+        private string housesalad = " House Salad ";
+        private string gardensalad = " Garden Salad ";
+        private string fruitsalad = " Fruit Salad ";
+        private string plainsalad = " Plain Salad ";
+        private string coke = " Coke ";
+        private string sprite = " Sprite ";
+        private string rootbeer = " Root Beer ";
+        private string tea = " Tea ";
         private int quantity;
+        public double runningTotal = 0D;
 
-        public string Food
+        public double Hamburgerprice()
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            return 15.75;
         }
-
-        public double Cost
+        public double doublepattyprice()
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
+            return 6.99;
         }
+        public double tomatoprice()
+        {
+            return 1.99;
+        }
+        public double onionprice()
+        {
+            return 1.99;
+        }
+        public double lettuceprice()
+        {
+            return 1.99;
+        }
+        public double pickleprice()
+        {
+            return 1.99;
+        }
+        public double housesaladprice()
+        {
+            return 5.99;
+        }
+        public double gardensaladprice()
+        {
+            return 9.99;
+        }
+        public double fruitsaladprice()
+        {
+            return 6.99;
+        }
+        public double plainsaladprice()
+        {
+            return 2.99;
+        }
+        public double cokeprice()
+        {
+            return 2.99;
+        }
+        public double spriteprice()
+        {
+            return 2.99;
+        }
+        public double rootbeerprice()
+        {
+            return 2.99;
+        }
+        public double teaprice()
+        {
+            return 1.99;
+        }
+        
 
         public int Quantity
         {
