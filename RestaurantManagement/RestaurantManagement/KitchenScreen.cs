f56225f0f50d125b0ca0ab1a5235bb7b45a94ff2
monkey <monkey@BB21012>
2017-05-04 18:38:44 -0500

    Update code

--- RestaurantManagement/RestaurantManagement/KitchenScreen.cs
+++ RestaurantManagement/RestaurantManagement/KitchenScreen.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Data;
 using System.Windows.Forms;
 
 namespace RestaurantManagement
@@ -25,6 +28,37 @@ namespace RestaurantManagement
         private void txtOrder1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            //Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password = myPassword;
+            connection.ConnectionString = "Server=cis1.actx.edu;Database=project1;User Id=db1;Password = db10;";
+            connection.Open();
+
+          
+            using (SqlCommand readAllOrderRecords = connection.CreateCommand())
+            {
+                readAllOrderRecords.CommandText = "select * from dbo.OrderTable;";
+
+                using (SqlDataReader reader = readAllOrderRecords.ExecuteReader())
+                {
+                    string rec = "";
+                    while (reader.Read())
+                    {
+                        rec = reader.GetString(1) + " \r\n";
+                        //rec += reader.GetString(2);
+                        
+                        txtOrder1.Text=rec;
+                    }
+                }
+            }
+        }
+
+        private void bntComplete_Click(object sender, EventArgs e)
+        {
+            txtOrder1.Text = string.Empty; 
         }
     }
 }
